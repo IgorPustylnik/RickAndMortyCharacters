1d4b53ab505bc7f1f119f5d4a9a18d79fe1d98a9
igorpustylnik <darthveider36@gmail.com>
2024-07-17 15:03:46 +0300

    Add missing font

--- RickAndMortyCharacters/Helpers/Fonts.swift
+++ RickAndMortyCharacters/Helpers/Fonts.swift
@@ -8,9 +8,44 @@
 import UIKit
 
 extension UIFont {
-    enum IBMPlexSans {
+    private enum IBMPlexSansTypes {
         static let regular = "IBMPlexSans-Regular"
         static let medium = "IBMPlexSans-Medium"
+        static let semiBold = "IBMPlexSans-SemiBold.ttf"
         static let bold = "IBMPlexSans-Bold"
+    }
+    
+    enum IBMPlexSans {
+        static func regular(size: CGFloat) -> UIFont {
+            guard let font = UIFont(name: IBMPlexSansTypes.regular, size: size) else {
+                print("Font \(IBMPlexSansTypes.regular) not found")
+                return .systemFont(ofSize: size)
+            }
+            return font
+        }
+        
+        static func medium(size: CGFloat) -> UIFont {
+            guard let font = UIFont(name: IBMPlexSansTypes.medium, size: size) else {
+                print("Font \(IBMPlexSansTypes.medium) not found")
+                return .systemFont(ofSize: size)
+            }
+            return font
+        }
+        
+        static func semiBold(size: CGFloat) -> UIFont {
+            guard let font = UIFont(name: IBMPlexSansTypes.semiBold, size: size) else {
+                print("Font \(IBMPlexSansTypes.semiBold) not found")
+                return .systemFont(ofSize: size)
+            }
+            return font
+        }
+        
+        static func bold(size: CGFloat) -> UIFont {
+            guard let font = UIFont(name: IBMPlexSansTypes.bold, size: size) else {
+                print("Font \(IBMPlexSansTypes.bold) not found")
+                return .systemFont(ofSize: size)
+            }
+            return font
+        }
     }
 }
