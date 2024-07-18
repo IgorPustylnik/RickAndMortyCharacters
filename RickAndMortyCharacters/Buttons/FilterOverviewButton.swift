9f07927428ca74891e6ffa120b4f13020531f9df
igorpustylnik <darthveider36@gmail.com>
2024-07-19 01:22:27 +0300

    Edit filter overview button

--- RickAndMortyCharacters/Buttons/FilterOverviewButton.swift
+++ RickAndMortyCharacters/Buttons/FilterOverviewButton.swift
@@ -10,59 +10,30 @@ import UIKit
 class FilterOverviewButton: UIButton {
 
     private var title: String
-    
-    private var isOn: Bool {
-            didSet {
-                updateConfig()
-            }
-        }
+    private var clickable: Bool
     
     private func updateConfig() {
         var config = UIButton.Configuration.bordered()
-        config.buttonSize = .medium
-        config.baseBackgroundColor = isOn ? .Colors.filterSelected : .clear
-        config.baseForegroundColor = isOn ? .Colors.background : .Colors.filterSelected
-        config.title = title
-        config.background.cornerRadius = 24
-        config.background.strokeWidth = isOn ? 0 : 2
-        config.background.strokeColor = .Colors.toggleButtonBorder
-        config.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 16, bottom: 10, trailing: 16)
+        config.buttonSize = .small
+        config.baseBackgroundColor = clickable ? .Colors.aqua : .Colors.filterSelected
+        config.baseForegroundColor = clickable ? .Colors.onColoredBackgroundText : .Colors.background
         
-        if isOn {
-            let image = UIImage(named: "icons/checkMark")?.withTintColor(.Colors.background, renderingMode: .alwaysOriginal)
-            config.image = image
-            config.imagePadding = 8
-            config.imagePlacement = .trailing
-        } else {
-            config.image = nil
-        }
-        config.attributedTitle = AttributedString(title, attributes: AttributeContainer([.font: UIFont.IBMPlexSans.regular(size: 12)]))
+        config.title = title
+        config.background.cornerRadius = 16.85
+        config.contentInsets = NSDirectionalEdgeInsets(top: 6.5, leading: 16, bottom: 6.5, trailing: 16)
+        config.attributedTitle = AttributedString(title, attributes: AttributeContainer([.font: UIFont.IBMPlexSans.regular(size: 8.43)]))
         
         self.configuration = config
     }
     
-    init(title: String, isOn: Bool) {
+    init(title: String, clickable: Bool) {
         self.title = title
-        self.isOn = isOn
+        self.clickable = clickable
         super.init(frame: .zero)
-        self.addTarget(self, action: #selector(toggleButtonTapped), for: .touchUpInside)
         updateConfig()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    @objc private func toggleButtonTapped() {
-        isOn.toggle()
-    }
-    
-    func setActive(_ active: Bool) {
-        self.isOn = active
-    }
-    
-    func getButtonState() -> Bool {
-        return isOn
-    }
-
 }
