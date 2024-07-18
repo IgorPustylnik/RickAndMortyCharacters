12d2578961ddef0740863942292f6b094832d8c5
igorpustylnik <darthveider36@gmail.com>
2024-07-19 01:24:29 +0300

    Create filter observer

--- RickAndMortyCharacters/Models/FilterModel.swift
+++ RickAndMortyCharacters/Models/FilterModel.swift
@@ -14,13 +14,35 @@ typealias FilterGender = [Gender: Bool]
 struct Filter {
     var status: FilterStatus
     var gender: FilterGender
-    
+
     static var initialState = Filter(status: [.alive: false, .dead: false, .unknown: false],
                                      gender: [.male: false, .female: false, .genderless: false, .unknown: false])
 }
 
+protocol FilterObserver: AnyObject {
+    func filterDidUpdate(_ filter: Filter)
+}
+
 class FilterModel {
     static var shared = FilterModel()
-    
-    var filter: Filter = Filter.initialState
+
+    private var observers = [FilterObserver]()
+
+    var filter: Filter = Filter.initialState {
+        didSet {
+            notifyObservers()
+        }
+    }
+
+    func addObserver(_ observer: FilterObserver) {
+        observers.append(observer)
+    }
+
+    private func notifyObservers() {
+        observers.forEach {
+            $0.filterDidUpdate(filter)
+        }
+    }
+
+    var searchQuery: String = ""
 }
