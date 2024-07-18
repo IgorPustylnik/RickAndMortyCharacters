80ee0a7e11009352a1f0f7f4eb6e27aed3e0783e
igorpustylnik <darthveider36@gmail.com>
2024-07-18 18:44:02 +0300

    Add custom buttons

---
+++ RickAndMortyCharacters/Views/Buttons/CustomAquaButton.swift
@@ -0,0 +1,20 @@
+//
+//  CustomAquaButton.swift
+//  RickAndMortyCharacters
+//
+//  Created by Игорь Пустыльник on 18.07.2024.
+//
+
+import UIKit
+
+class CustomAquaButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
