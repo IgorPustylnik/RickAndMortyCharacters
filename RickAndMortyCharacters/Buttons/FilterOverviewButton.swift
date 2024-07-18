a3fab4038b7fdeb89593752f9d68bbe401f2216c
igorpustylnik <darthveider36@gmail.com>
2024-07-18 22:02:30 +0300

    Edit project structure

---
+++ RickAndMortyCharacters/Buttons/FilterOverviewButton.swift
@@ -0,0 +1,68 @@
+//
+//  FilterOverviewButton.swift
+//  RickAndMortyCharacters
+//
+//  Created by Игорь Пустыльник on 18.07.2024.
+//
+
+import UIKit
+
+class FilterOverviewButton: UIButton {
+
+    private var title: String
+    
+    private var isOn: Bool {
+            didSet {
+                updateConfig()
+            }
+        }
+    
+    private func updateConfig() {
+        var config = UIButton.Configuration.bordered()
+        config.buttonSize = .medium
+        config.baseBackgroundColor = isOn ? .Colors.filterSelected : .clear
+        config.baseForegroundColor = isOn ? .Colors.background : .Colors.filterSelected
+        config.title = title
+        config.background.cornerRadius = 24
+        config.background.strokeWidth = isOn ? 0 : 2
+        config.background.strokeColor = .Colors.toggleButtonBorder
+        config.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 16, bottom: 10, trailing: 16)
+        
+        if isOn {
+            let image = UIImage(named: "icons/checkMark")?.withTintColor(.Colors.background, renderingMode: .alwaysOriginal)
+            config.image = image
+            config.imagePadding = 8
+            config.imagePlacement = .trailing
+        } else {
+            config.image = nil
+        }
+        config.attributedTitle = AttributedString(title, attributes: AttributeContainer([.font: UIFont.IBMPlexSans.regular(size: 12)]))
+        
+        self.configuration = config
+    }
+    
+    init(title: String, isOn: Bool) {
+        self.title = title
+        self.isOn = isOn
+        super.init(frame: .zero)
+        self.addTarget(self, action: #selector(toggleButtonTapped), for: .touchUpInside)
+        updateConfig()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc private func toggleButtonTapped() {
+        isOn.toggle()
+    }
+    
+    func setActive(_ active: Bool) {
+        self.isOn = active
+    }
+    
+    func getButtonState() -> Bool {
+        return isOn
+    }
+
+}
---
+++ RickAndMortyCharacters/Buttons/PlainImageButton.swift
@@ -0,0 +1,52 @@
+//
+//  PlainImageButton.swift
+//  RickAndMortyCharacters
+//
+//  Created by Игорь Пустыльник on 18.07.2024.
+//
+
+import UIKit
+
+class PlainImageButton: UIButton {
+    
+    private var imageName: String
+    private var size: UIButton.Configuration.Size
+    
+    private var active: Bool {
+        didSet {
+            updateConfig()
+        }
+    }
+    
+    private func updateConfig() {
+        var config = UIButton.Configuration.borderless()
+        config.buttonSize = size
+        config.baseBackgroundColor = .clear
+        config.baseForegroundColor = active ? .Colors.aqua : .Colors.mainText
+        
+        config.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
+        
+        config.image = UIImage(named: imageName)?.withTintColor(.Colors.background, renderingMode: .alwaysOriginal)
+        config.imagePlacement = .all
+        
+        self.configuration = config
+    }
+
+    
+    init(imageName: String, active: Bool, size: UIButton.Configuration.Size) {
+        self.active = active
+        self.imageName = imageName
+        self.size = size
+        super.init(frame: .zero)
+        updateConfig()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setActive(_ active: Bool) {
+        self.active = active
+    }
+
+}
